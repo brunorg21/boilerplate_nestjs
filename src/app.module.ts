2d8b492b53f904c64a6dd0f53e4f5f7225e4d1f3
brunorg21 <rbrunog21@gmail.com>
2025-06-10 22:36:38 -0300

    feat: add new modules on app

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,9 @@ import { NoAuthModule } from './modules/no-auth/no-auth.module';
 import { AdminModule } from './modules/admin/admin.module';
 import { MobileModule } from './modules/mobile/mobile.module';
 import { WebModule } from './modules/web/web.module';
+import { CompaniesModule } from './modules/companies/companies.module';
+import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
   imports: [
@@ -29,6 +32,9 @@ import { WebModule } from './modules/web/web.module';
     AdminModule,
     MobileModule,
     WebModule,
+    CompaniesModule,
+    UsersModule,
+    ProductsModule,
   ],
   providers: [
     { provide: APP_GUARD, useClass: ThrottlerGuard },
